751d3b64c05bb851d933033f5ff63de70220dc18
ko.libri <entengelb@gmail.com>
2026-03-04 23:46:42 +0100

    readme

--- run.sh
+++ run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 set -e
-
-NO_DOCKER=${NO_DOCKER:0}
-
 _target() {
     case $1 in
         help) ## displays help
@@ -11,7 +8,7 @@ _target() {
             echo "## Available targets:"
             grep -E '\)\s*##' $fn | sed "s|^[[:space:]]*\(.*\)) #\{2\} \(.*\)|\1: \2|g"
         ;;
-        python-init) ## initialice python venv
+        python-init) ## initialize python venv
             python -m venv .venv
             source .venv/bin/activate
             .venv/bin/pip install --no-cache-dir -r requirements.txt
@@ -20,32 +17,24 @@ _target() {
             .venv/bin/python sheeter.py
         ;;
         sheets-docker) ## build characters in docker
-            _compose_run sheeter python sheeter.py
+            echo "compose run: ${*:2}"
+            docker compose run --remove-orphans sheeter python sheeter.py
         ;;
-        container-build) ## builds docker container
+        container-build) ## build docker container
             docker compose build
         ;;
         *)
-    # backup target: tries to call one of the tools-function
+
     if [ -z "${@:2}" ]; then
+        echo "No target found."
+        echo "Try this:"
+        echo "cat README.md"
+        echo ""
         _target help
         exit 0
     fi
-
-    TOOLS=`declare -F | sed -r 's/declare -f _(.*)/\1/g'`
-    if [[ ${TOOLS[*]} =~ $1 ]]; then
-        _"$1" "${@:2}"
-    else
-        echo "Target not found: " $1
-    fi
     ;;
     esac
-}
-
-# tools definition
-_compose_run() {
-    echo "compose run: ${*:2}"
-    docker compose run --remove-orphans "$1" "${@:2}"
 }
 
 _target "$@"
